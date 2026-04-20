c19710c4cecc8f71203fbfb3405ab870298252d7
KhThB <3123411034@sv.sgu.edu.vn>
2026-04-20 23:08:14 +0700

    mobile platform

---
+++ TourGuide.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TourGuide.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TourGuide.Mobile/Views/MapPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace TourGuide.Mobile.Views;
+
+public partial class MapPage : ContentPage
+{
+	public MapPage()
+	{
+		InitializeComponent();
+	}
+}
